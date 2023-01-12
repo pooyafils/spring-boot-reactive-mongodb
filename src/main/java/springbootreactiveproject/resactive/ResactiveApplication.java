c81719e485c7cd9bb3272a83edcf6fc468f9f195
pooyafils <pooyafils@gmail.com>
2023-01-12 10:33:35 -0800

    init commit

---
+++ src/main/java/springbootreactiveproject/resactive/ResactiveApplication.java
@@ -0,0 +1,13 @@
+package springbootreactiveproject.resactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springbootreactiveproject/resactive/ResactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootreactiveproject.resactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
