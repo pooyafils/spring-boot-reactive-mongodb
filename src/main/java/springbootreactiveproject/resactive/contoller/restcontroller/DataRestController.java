848859f445dd62d2a9600633d0ac22bb39e4f9d4
pooyafils <pooyafils@gmail.com>
2023-01-21 16:24:19 +0330

    adding rest crud contoller

--- src/main/java/springbootreactiveproject/resactive/contoller/restcontroller/DataRestController.java
+++ src/main/java/springbootreactiveproject/resactive/contoller/restcontroller/DataRestController.java
@@ -4,11 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+import springbootreactiveproject.resactive.model.Product;
 import springbootreactiveproject.resactive.repository.ProductRepository;
-
+@Transactional
 @RestController
 @RequestMapping("/rest")
 public class DataRestController {
@@ -19,4 +20,31 @@ public class DataRestController {
 
         return new ResponseEntity(productRepository.findAll(),HttpStatus.OK);
     }
+    @GetMapping("/findPrice")
+    public ResponseEntity findByPrice(@RequestParam int Price){
+
+        return new ResponseEntity(productRepository.findByPrice(new Integer(Price)),HttpStatus.OK);
+    }
+    @GetMapping("/findDescription")
+    public ResponseEntity findByDiscrption(@RequestParam String dis){
+        return new ResponseEntity<>(productRepository.findByDescription(dis),HttpStatus.OK);
+    }
+    @PostMapping("/save")
+    public ResponseEntity saved(@RequestBody Product product){
+productRepository.save(product).subscribe();
+return new ResponseEntity("saved",HttpStatus.OK);
+    }
+    @PostMapping("/update")
+    public ResponseEntity update(@RequestBody Product productUpdate){
+        //TODO - additional id match
+        Mono<Product> updatedCont = productRepository.findById(productUpdate.getId())
+                        .flatMap(oldContact -> {
+                            oldContact
+                                    .setPrice(120);
+                            return productRepository.save(oldContact);
+                        });
+      updatedCont.subscribe();
+       return ResponseEntity.ok("ok");
+    }
+
 }
